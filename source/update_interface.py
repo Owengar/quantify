5178352e8dde4ef016f89a8b7357dc669def298f
Owengar <owengaalt@gmail.com>
2024-09-07 16:19:14 -0500

    pre_update_commit

--- source/update_interface.py
+++ source/update_interface.py
@@ -20,6 +20,7 @@ txt_height = 110
 
 def event_loop(updates):
     global txt_height
+    print(updates)
     updates_surf = pat_font.render(updates, antialias=True, color=black, wraplength=490)
     print("no")
     for event in pygame.event.get():
