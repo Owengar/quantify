1b1229f0a4b6255c5e2d53afdd78ecbbe71ddea1
Owengar <owengaalt@gmail.com>
2024-09-07 14:22:15 -0500

    update_commit

--- source/update_interface.py
+++ source/update_interface.py
@@ -31,7 +31,7 @@ def event_loop(updates):
                 sys.exit()
         if event.type == pygame.MOUSEWHEEL:
             txt_height -= event.y * 15
-            if txt_height + 110 > 140:
+            if txt_height + 110 > 80:
                 txt_height += event.y *15
             elif txt_height <  -updates_surf.size[1] + 410:
                 txt_height += event.y * 15
