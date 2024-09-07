6bc7a36d4772d634d3e519ea0c8cee1876d21e28
Owengar <owengaalt@gmail.com>
2024-09-07 14:38:16 -0500

    commetear

--- source/git_initer.ps1
+++ source/git_initer.ps1
@@ -2,5 +2,5 @@
 git init -b main
 git switch main
 git add .
-git commit -m update_commit
+git commit -m pre_update_commit
 git fetch git@github.com:Owengar/quantify.git main:latest
--- source/open_quantify.py
+++ source/open_quantify.py
@@ -18,6 +18,7 @@ def check_for_updates(open_folder, source_dir):
         while True:
             if update_interface.event_loop(updates):
                 os.system("git checkout latest source")
+                os.system("git commit -m update_commit")
 
     os.system("git branch latest -D")
 
