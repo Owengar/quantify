eb9c18d1c59dd47d8f452caebae87a143be7c155
Owengar <owengaalt@gmail.com>
2024-09-07 14:34:11 -0500

    fixed checkout

--- source/open_quantify.py
+++ source/open_quantify.py
@@ -17,7 +17,7 @@ def check_for_updates(open_folder, source_dir):
     if len(updates):
         while True:
             if update_interface.event_loop(updates):
-                os.system("git checkout latest -- \\source")
+                os.system("git checkout latest source")
 
     os.system("git branch latest -D")
 
