d9141f09d5b091f0c7be84cde1628d3b4c1c88af
Owengar <owengaalt@gmail.com>
2024-09-08 09:14:58 -0500

    pre_update_commit

--- source/update_interface.py
+++ source/update_interface.py
@@ -48,7 +48,7 @@ def event_loop(updates):
         def render_updates(txt):
             updates_surf = pat_font.render(txt, antialias=True, color=black, wraplength=490)
         print("before")
-        update_text_thread = subprocess.run(render_updates, args=[updates])
+        update_text_thread = subprocess.run(render_updates)
         start_time = time.time()
         update_text_thread.start()
         print("after")
