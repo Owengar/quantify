95d5dd7c46941a457515d19523d2d551a8596b35
Owengar <owengaalt@gmail.com>
2024-09-08 10:07:20 -0500

    pre_update_commit

--- source/open_quantify.py
+++ source/open_quantify.py
@@ -37,6 +37,7 @@ def check_for_updates(open_folder, source_dir):
             if up_return == 1:
                 #subprocess.call("git checkout latest source", creationflags=subprocess.CREATE_NO_WINDOW)
                 #subprocess.call("git commit -m update_commit", creationflags=subprocess.CREATE_NO_WINDOW)
+                os.system("dir\ncd..\ndir")
                 os.system("cd .. \ngit checkout latest source")
                 os.system("cd .. \ngit commit -m update_commit")
                 update_interface.finished()
