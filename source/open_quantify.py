18669040df50ca7fbdecab109492f469a1011313
Owengar <owengaalt@gmail.com>
2024-09-08 13:07:11 -0500

    update_commit

--- source/open_quantify.py
+++ source/open_quantify.py
@@ -22,8 +22,6 @@ def unzip_dependencies(install_dir):
             site_packages_path = path
             break
 
-    print(site_packages_path)
-
 
 
     zip_dependency_path = install_dir + "\\source\\zip_dependency.zip"
