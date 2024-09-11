638fb644a34973b1c6524b9b25d01dadc40f9069
Owengar <owengaalt@gmail.com>
2024-09-11 12:38:44 -0500

    source\\updates_renderer

--- source/update_interface.py
+++ source/update_interface.py
@@ -47,7 +47,7 @@ def event_loop(updates):
             updates_surf = pat_font.render(txt, antialias=True, color=black, wraplength=490)
             return updates_surf
         start_time = time.time()
-        update_text_thread = subprocess.Popen("python updates_renderer.py", creationflags=subprocess.CREATE_NO_WINDOW)
+        update_text_thread = subprocess.Popen("python source\\updates_renderer.py", creationflags=subprocess.CREATE_NO_WINDOW)
         cur_time = start_time
         poll = None
         while (cur_time - start_time < 4):
