e12f074dbf829385f5c511dfe7e91278aa1bbc2b
Owengar <owengaalt@gmail.com>
2025-06-09 10:40:45 -0500

    Documented and fixed make_setpoint_list

--- source/make_setpoint_list.py
+++ source/make_setpoint_list.py
@@ -1,12 +1,13 @@
-from source.runner import run
 import numpy
-from source.make_setpoint_list import make_setpoint_list
 
+def make_setpoint_list(ranges : list[tuple[float, float, int]]):
+    """This is a helper function to assemble a setpoint list that is assembled in parts.
+    
+    The "ranges" parameter is a list of (start, stop, step_count) tuples, identical to what is inputted into the numpy.linspace function.
+    
+    All ranges in the list will be concatenated together into one numpy array and then returned."""
+    base_list = []
+    for setpoint_range in ranges:
+        base_list.extend(list(numpy.linspace(setpoint_range[0], setpoint_range[1], setpoint_range[2])))
 
-import sys
-print(numpy.linspace(-2, 2, 10))
-print(make_setpoint_list([(-2, 2, 10)]))
-
-run("example_1d_script", [make_setpoint_list([(-2, 2, 10), (2, -2, 10)])])
-
-
+    return numpy.array(base_list)
